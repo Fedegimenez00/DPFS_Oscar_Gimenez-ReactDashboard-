ede1bfaf100f35297b3190a8e1fc02d3553a69c3
Fedegimenez00 <federicogimenez99@hotmail.com>
2025-05-30 13:11:24 -0300

    fixed seeders & added more images to courses

--- express-app/src/database/seeders/20250422033052-Subcategories.js
+++ express-app/src/database/seeders/20250422033052-Subcategories.js
@@ -7,53 +7,64 @@ module.exports = {
       [
         
         // Seguridad Ofensiva
-        { name: "Hacking de Aplicaciones Web",
+        { id: 1,
+          name: "Hacking de Aplicaciones Web",
           description: 'Descubre y explota fallos en aplicaciones web',
           category_id: 2 
         },
-        { name: "Pentesting", 
+        { id: 2,
+          name: "Pentesting", 
           description: 'Evalúa sistemas simulando ataques reales',
           category_id: 2 
         },
-        { name: "Explotación de vulnerabilidades", 
+        { id: 3,
+          name: "Explotación de vulnerabilidades", 
           description: 'Diseña y ejecuta exploits avanzados',
           category_id: 2 
         },
         
         // Seguridad Defensiva
-        { name: "Fortificación de Redes", 
+        { id: 4,
+          name: "Fortificación de Redes", 
           description: 'Protege sistemas mediante configuraciones seguras',
           category_id: 3 
         },
-        { name: "Respuesta a Incidentes", 
+        { id: 5,
+          name: "Respuesta a Incidentes", 
           description: 'Actúa ante ciberataques en tiempo real',
           category_id: 3 
         },
-        { name: "Análisis de Malware", 
+        { id: 6,
+          name: "Análisis de Malware", 
           description: 'Detecta y desarma software malicioso',
           category_id: 3 
         },
         
         // Seguridad de Nube
-        { name: "Gestión de Identidades y Accesos (IAM)", 
+        { id: 7,
+          name: "Gestión de Identidades y Accesos (IAM)", 
           description: 'Controla accesos y permisos en la nube',
           category_id: 4 
         },
-        { name: "Arquitectura Segura en la Nube", 
+        { id: 8,
+          name: "Arquitectura Segura en la Nube", 
           description: 'Diseña infraestructuras cloud resistentes',
           category_id: 4 
         },
-        { name: "Respuestas a Incidentes en Cloud", 
+        { id: 9,
+          name: "Respuestas a Incidentes en Cloud", 
           description: 'Mitiga amenazas específicas de entornos cloud',
           category_id: 4 
         },
 
         // Ingeniería Social
-        { name: "Técnicas de Ingeniería Social", 
+        { id: 10,
+          name: "Técnicas de Ingeniería Social", 
           description: 'Aprende estrategias de manipulación y engaño',
           category_id: 5 
         },
-        { name: "Concientización y Educación", 
+        { id: 11,
+          name: "Concientización y Educación", 
           description: 'Fortalece seguridad con capacitación preventiva',
           category_id: 5 
         },
